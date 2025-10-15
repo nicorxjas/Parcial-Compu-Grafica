1c876ea6b26c893c72370dc17f89ac318cedb6c9
Nico <2215886@ucc.edu.ar>
2025-10-14 23:19:10 -0300

    hitbbo

--- src/hit.py
+++ src/hit.py
@@ -1,17 +1,24 @@
 import glm 
 
 class Hit:
-    def __init__(self, position=(0,0,0), scale=(1,1,1)):
-        self.__position = glm.vec3(*position)
-        self.__scale = glm.vec3(*scale)
+    def __init__(self, get_model_matrix):
+        self.__model_matrix = get_model_matrix
+
+    @property
+    def model_matrix(self):
+        return self.__model_matrix()
 
     @property
     def position(self):
-        return self.__position
+        m = self.model_matrix
+        return glm.vec3(m[3].x, m[3].y, m[3].z)
     
     @property
     def scale(self):
-        return self.__scale
+        m = self.model_matrix
+        return glm.vec3(glm.lenght(glm.vec3(m[0])),
+                        glm.lenght(glm.vec3(m[1])),
+                        glm.lenght(glm.vec3(m[2])))
     
     def check_hit(self, origin, direction):
         raise NotImplementedError("Subclasses must implement this method")
@@ -36,4 +43,33 @@ class HitBox(Hit):
         t_near = max(t1.x, t1.y, t1.z)
         t_far = min(t2.x, t2.y, t2.z)
 
+        return t_near <= t_far and t_far >= 0
+    
+class HitBoxOBB(Hit):
+    def __init__(self, get_model_matrix):
+          super().__init__(get_model_matrix)
+
+    def check_hit(self, origin, direction):
+        origin = glm.vec3(origin)
+        direction = glm.normalize(glm.vec3(direction))
+
+        inv_model = glm.inverse(self.model_matrix)
+        local_origin = inv_model * glm.vec4(origin, 1.0)
+        local_dir = inv_model * glm.vec4(direction, 0.0)
+
+        local_origin = glm.vec3(local_origin)
+        local_dir = glm.normalize(glm.vec3(local_dir))
+
+        min_bounds = glm.vec3(-1, -1, -1)
+        max_bounds = glm.vec3(1, 1, 1)
+
+        tmin = (min_bounds - local_origin) / local_dir
+        tmax = (max_bounds - local_origin) / local_dir
+
+        t1 = glm.min(tmin, tmax)
+        t2 = glm.max(tmin, tmax)
+
+        t_near = max(t1.x, t1.y, t1.z)
+        t_far = min(t2.x, t2.y, t2.z)
+        
         return t_near <= t_far and t_far >= 0
